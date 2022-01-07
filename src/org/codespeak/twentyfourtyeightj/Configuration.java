6e3729ef3a4151306110119de0d1a7664d7cf95c
MisterVector <thecodespeaker@outlook.com>
2022-01-06 19:19:27 -0800

    Added a configuration class

---
+++ src/org/codespeak/twentyfourtyeightj/Configuration.java
@@ -0,0 +1,14 @@
+package org.codespeak.twentyfourtyeightj;
+
+/**
+ * A class that deals with the configuration of the program
+ *
+ * @author Vector
+ */
+public class Configuration {
+    
+    public static final String PROGRAM_VERSION = "0.0.0";
+    public static final String PROGRAM_NAME = "2048j";
+    public static final String PROGRAM_TITLE = PROGRAM_NAME + " v" + PROGRAM_VERSION;
+
+}
